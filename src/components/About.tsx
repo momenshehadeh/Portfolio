54ee8b15f34ae55161d9b29afc3bbff5449765ed
Momen Shehadeh <141729731+momenshehadeh@users.noreply.github.com>
2025-06-20 16:17:48 +0300

    About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,10 +49,9 @@ export default function About() {
         <div className="text-center mb-16">
           <h2 className="text-4xl text-indigo-600 mb-4">Overview</h2>
           <p className="text-gray-800 max-w-4xl mx-auto">
-            Computer Science graduate with proven ability to adapt in both independent and collaborative environments, 
-            consistently delivers high-quality solutions under tight deadlines. Specializes in front-end development 
-            with a focus on web applications. Skilled at solving problems and improving processes to achieve efficiency 
-            and build scalable solutions.
+           Motivated and detail-oriented Computer Science graduate seeking an entry-level software development
+position where I can apply my strong programming skills, problem-solving abilities, and passion for technology
+to contribute to innovative projects and grow professionally within a dynamic team environment.
           </p>
         </div>
 
@@ -60,19 +59,25 @@ export default function About() {
           <Specialization
             title="Front-end Developer"
             icon={<img src="https://cdn-icons-png.flaticon.com/512/5339/5339181.png" alt="Front-end Development" className="w-10 h-10" />}
-            description="Expertise in building responsive web applications using modern frameworks and best practices for optimal user experience."
+            description="A front-end developer builds the visual and interactive parts of websites and web applications.
+They use HTML, CSS, JavaScript and React to create user-friendly, responsive designs that work across devices and browsers."
           />
           <Specialization
-            title="Cybersecurity"
-            icon={<img src="https://img.icons8.com/?size=100&id=VP08IIvzEIe2&format=png&color=000000" alt="Cybersecurity" className="w-10 h-10" />}
-            description="Knowledge of security principles, threat analysis, and implementing secure coding practices in web applications."
+            title="Back-end Developer"
+            icon={<img src="https://cdn-icons-png.flaticon.com/512/6213/6213731.png" alt="Back-end Developer" className="w-10 h-10" />}
+            description="A back-end developer works on the server-side of websites and applications.
+They build and manage databases, servers, and APIs, ensuring everything runs smoothly behind the scenes and supports the front-end with data and functionality."
           />
         </div>
 
         <div className="text-center mb-16">
           <h2 className="text-4xl text-indigo-600 mb-6">Work Experience</h2>
           <p className="text-gray-800 max-w-4xl mx-auto">
-          I have experience in web design, having worked on several projects that showcase my skills in creating visually appealing and user-friendly websites. My experience includes designing responsive layouts, improving user interfaces, and integrating modern web technologies. Through these projects, I have developed a strong understanding of the aesthetic and functional aspects of web design. I am passionate about delivering high-quality work that meets client expectations and enhances user experiences. This hands-on experience has honed my ability to manage projects efficiently and stay up-to-date with the latest design trends.
+          I have experience in web design, having worked on several projects that showcase my skills in creating visually appealing and user-friendly websites.
+My experience includes designing responsive layouts, improving user interfaces, and integrating modern web technologies.
+Through these projects, I have developed a strong understanding of the aesthetic and functional aspects of web design.
+I am passionate about delivering high-quality work that meets client expectations and enhances user experiences. 
+This hands-on experience has honed my ability to manage projects efficiently and stay up-to-date with the latest design trends.
           </p>
         </div>
 
@@ -82,16 +87,17 @@ export default function About() {
             <Experience
               company="Stack Soft Intern"
               role="Frontend Developer"
-              period="May 2024 - Sep 2024"
+              period="June 2024 - Sep 2024"
               description="Developed and maintained responsive web applications using React and TypeScript. Implemented modern UI/UX designs and improved application performance."
               icon={<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBkBENlDi7Ron4OTBeGTjyTqrs4x0l5-6TFA&s" alt="Stack Soft" className="w-10 h-10" />}
             />
             <Experience
-              company="Green Circle Intern"
-              role="Cyber Security"
-              period="Dec 2024 - Present"
-              description="Conducted security assessments and implemented security measures for web applications. Participated in threat modeling and vulnerability assessments."
-              icon={<img src="https://grcico.com/wp-content/uploads/2020/04/green-circle-approved-01.png" alt="Green Circle" className="w-10 h-10" />}
+              company="Idel Innovation House Intern"
+              role="Backend Developer"
+              period="Nov 2024 - May 2025"
+              description="developer works on the server-side of websites and applications.
+They build and manage databases, servers, and APIs, ensuring everything runs smoothly behind the scenes and supports the front-end with data and functionality."
+              icon={<img src="https://media.licdn.com/dms/image/v2/D4D0BAQHV0KPQfYEZjA/company-logo_200_200/company-logo_200_200/0/1725194167057/iih_ai_logo?e=2147483647&v=beta&t=C9DeRjP5t7nISQctsvdQmzFatkPJSLqNh40prTPBZlQ" alt="Idel Innoviation House" className="w-10 h-10" />}
             />
           </div>
         </div>
